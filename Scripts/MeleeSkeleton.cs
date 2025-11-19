bb9d1ab29440d36187bea53136d27c659a059e2b
Andrew R <Mydrewdog64@gmail.com>
2025-11-19 15:55:31 -0500

    Almost completed Skeleton

--- Scripts/MeleeSkeleton.cs
+++ Scripts/MeleeSkeleton.cs
@@ -1,32 +1,74 @@
 using Godot;
 using System;
+using System.Runtime.CompilerServices;
 
 enum EnemyState
 {
 	Idle,
 	Chase,
+	Attack,
 }
 
 public partial class MeleeSkeleton : Enemy
 {
+	//General Enemy Variables
 	private EnemyState State = EnemyState.Idle;
-	private Vector2 Direction = new Vector2(0,0);
+	private Vector2 direction = new Vector2(0,0);
+
+	//Attacking Variables
+	private int to_hit_lag = 0;
+	private int hit_timer = 0;
+	bool has_attacked = false;
+
+	bool player_got_away;
+
+	Entity player;
 	public void when_player_enters(Node2D body)
 	{
-		GD.Print("h");
+		player = (Entity)body;
+		
 		if (body is Player p)
 		{
-			Direction = (p.GlobalPosition - this.GlobalPosition).Normalized();
+			
 			State = EnemyState.Chase;
+			
+			
+
 		}
 	}
 	
+	public void on_attack_enter(Node2D body)
+    {
+		GD.Print("ENter");
+		player_got_away = false;
+	
+        if(body is Player p && !has_attacked)
+        {
+			to_hit_lag = 30;
+            State = EnemyState.Attack;
+        }
+    }
+
+	public void on_attack_exit(Node2D body)
+    {
+		GD.Print("EXIT");	
+        if(body is Player p && to_hit_lag <= 0)
+        {
+			
+            State = EnemyState.Chase;
+        }
+        else
+        {
+            player_got_away = true;
+        }
+    }
 	public void when_player_exits(Node2D body)
 	{
-		GD.Print("g");
+		
         if(body is Player p)
         {
 			State = EnemyState.Idle;
+			GD.Print(State);
         }
     }
 	// Called when the node enters the scene tree for the first time.
@@ -39,13 +81,83 @@ public partial class MeleeSkeleton : Enemy
 	{
 		if (State == EnemyState.Chase)
 		{
-			Velocity = speed * Direction;
+			direction = (player.GlobalPosition - this.GlobalPosition).Normalized();
+			Velocity = speed * direction;
+			animator.Set("parameters/Walk/blend_position", direction);
+			has_attacked = false;
+			
+			
 		}
 		else if (State == EnemyState.Idle)
 		{
 			Velocity = new Vector2(0, 0);
+			
 		}
+		else if (State == EnemyState.Attack)
+        {
+			to_hit_lag--;
+			
+            Velocity = new Vector2(0,0);
+			direction = (player.GlobalPosition - this.GlobalPosition).Normalized();
+			animator.Set("parameters/Attack/blend_position", direction);
+
+			if(to_hit_lag <= 0 && !player_got_away && !has_attacked)
+            {
+				//Swing and Hits
+				GD.Print("Swing and Hit");
+				player.TakeDamage(attack,false,direction);
+				hit_timer = 30;
+				has_attacked = true;
+                //Proc Damage and add hitlag has_attacked = true
+            }else if(to_hit_lag <=0 && player_got_away && !has_attacked){
+                //Swing and misses
+				GD.Print("Swing and Miss");
+				hit_timer = 30;
+				has_attacked = true;
+				
+            }
+
+			hit_timer --;
+			if(hit_timer<=0 && has_attacked && player_got_away)
+            {
+				GD.Print("CHASE HE GOT AWAY");
+				//Default Chase Player after missing attack
+                State = EnemyState.Chase;
+            }
+            else if(hit_timer<=0 && to_hit_lag<=0)
+            {
+				GD.Print("ATTACK AGAIN");
+				//Edge Case for when the player re enters during hit lag
+                to_hit_lag = 30;
+				has_attacked = false;
+				player_got_away = false;
+				State = EnemyState.Attack;
+				
+            }
+
+        }
+
+		
+            
+        
+        
+		
 
 		MoveAndSlide();
+    }
+
+	public bool IsIdle()
+    {
+        return State == EnemyState.Idle;
+    }
+
+    public bool IsChasing()
+    {
+        return State == EnemyState.Chase;
+    }
+
+	public bool IsAttacking()
+    {
+        return State == EnemyState.Attack;
     }
 }
