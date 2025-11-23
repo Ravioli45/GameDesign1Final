e8b3e1d9f49e0d4abcaf5ae85b07db4c115e4b67
Andrew R <Mydrewdog64@gmail.com>
2025-11-23 03:04:35 -0500

    Almost Completed Boss Missing the ability to Melee Attack in his "Walking Phase"

---
+++ Scripts/Alistar.cs
@@ -0,0 +1,280 @@
+using Godot;
+using System;
+using System.Data;
+
+enum BossState
+{
+    Idle,
+	Projectile,
+	Walking,
+	Attacking,
+	Charging,
+	Tackle,
+}
+public partial class Alistar : CharacterBody2D
+{
+    [Export] AnimationTree Animator;
+    [Export] PackedScene Fireball;
+
+	private Player PlayerNode;
+	private Vector2 Direction = new Vector2(0,0);
+    private Vector2 StaticDirection = new Vector2(0,0);
+    bool UnlockDirection = true;
+	private int AttackSelection = -1;
+	private BossState State = BossState.Idle;
+    private bool InAttack = false;
+    private int Damage = 1;
+    private int ChargeDamage = 2;
+    public bool fightStarted = false;
+
+    private bool InMeleeRange = false;
+    private bool MeleeAttacking = false;
+   
+
+	public void PlayerEntersFight(Node2D body)
+    {
+       
+        if(body is Player p){
+        CollisionShape2D BossPlayerDetection = GetNode<CollisionShape2D>("PlayerDetectionHandeler/PlayerDetection");
+        BossPlayerDetection.SetDeferred("disabled",false);
+        
+        fightStarted = true;
+        
+        }
+    }
+
+	public void OnDetectionEnter(Node2D body)
+    {
+		if (body is Player p)
+		{
+		
+            PlayerNode = p;
+			Direction = (p.GlobalPosition - this.GlobalPosition).Normalized();
+
+			
+		}
+        
+    }
+
+    public void PlayerEntersMelee(Node2D body)
+    {
+        if(body is Player p){
+        InMeleeRange = true;
+        }
+    }
+
+    public void PlayerExitsMelee(Node2D body)
+    {
+        if(body is Player p){
+        InMeleeRange = false;
+        }
+    }
+	//Vulnerable represents the states the boss can be in seperated by Idle or vulnerable phases
+	public async void Vulnerable()
+    {
+        
+
+		//Vulnerability frames for player to attack
+		await ToSignal(GetTree().CreateTimer(2f), "timeout");
+        if(AttackSelection == 0)
+        {
+			//Begins his Charge tackle attack
+            
+        	State = BossState.Charging;
+            //Stalls for the Kicking Animation
+            await ToSignal(GetTree().CreateTimer(1.5f), "timeout");
+            //Calls 
+            State = BossState.Tackle;
+			
+        } else if(AttackSelection == 1)
+        {
+			//Begins his walking/melee attack cycle
+			State = BossState.Walking;
+            //Walks for 5 seconds before changing states CURRENTLY NON FUNCTIONAL
+            await ToSignal(GetTree().CreateTimer(5f), "timeout");
+            
+            State = BossState.Idle;
+            InAttack = false;
+            
+        } else if(AttackSelection == 2)
+        {
+            //Begins his tantrum attack
+            State = BossState.Projectile;
+
+            //Handles the dirt particles
+            CpuParticles2D particles = GetNode<CpuParticles2D>("GroundPound");
+		    particles.Emitting = true;
+		    particles.Visible = true;
+		    particles.Restart();
+
+            //Shoots 5 Projectiles with 0.5 seconds in between each
+            for(int i = 0; i < 5 ; i++){
+            await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
+            Direction = (PlayerNode.GlobalPosition - this.GlobalPosition).Normalized();
+            StaticDirection = Direction;
+            ProjectileAttack(StaticDirection);
+            }
+
+            //Makes particles dissapear
+            particles.Visible = false;
+		    particles.Emitting = false;
+
+            State = BossState.Idle;
+            InAttack = false;
+
+            
+        }
+        //Resets back to first type of attack
+        if(AttackSelection == 2)
+        {
+            AttackSelection = -1;
+        }
+    }
+
+	public async void Tackle(Vector2 StaticDirection)
+    {
+            //Charges in a set direction, stops when hits player or anything else
+            Velocity = 300 * StaticDirection;
+            MoveAndSlide();
+           for (int i = 0; i < GetSlideCollisionCount(); i++)
+            {
+                var collision = GetSlideCollision(i);
+                if (collision.GetCollider() is Player p)
+                {
+                    
+                    p.TakeDamage(ChargeDamage,false);
+                    InAttack = false;
+					State = BossState.Idle;
+                    
+                    break;
+                }
+            }
+
+        	if (GetSlideCollisionCount()>0)
+        	{
+                
+            	State = BossState.Idle;
+                 InAttack = false;
+        	}
+			
+    }
+
+    public async void MeleeAttack()
+    {
+        //Not Functional Await functions bug out the timer in the vulnerable function
+            MeleeAttacking = true;
+            State = BossState.Attacking;
+            //Stall to hit ground first
+            await ToSignal(GetTree().CreateTimer(0.5f), "timeout");
+            if (InMeleeRange)
+            {
+                PlayerNode.TakeDamage(Damage,false);
+            }
+            await ToSignal(GetTree().CreateTimer(1f), "timeout");
+            MeleeAttacking = false;
+            State = BossState.Walking;
+        
+    }
+
+    public async void ProjectileAttack(Vector2 StaticDirection)
+    {
+        //Spawns Projectiles
+        
+        CharacterBody2D instance = Fireball.Instantiate<CharacterBody2D>();
+        instance.Position = this.Position;
+        instance.Rotation = Mathf.Atan2(StaticDirection.Y , StaticDirection.X);
+		instance.Velocity = StaticDirection *200;
+        GetTree().Root.AddChild(instance);
+    }
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+        GD.Print(State);
+        
+        if(fightStarted){
+
+		if(State == BossState.Idle)
+        {
+            //Every Action Starts and ends in IDLE
+            //Each State needs to begin with InAttack = True to not go back to idle
+            if(!InAttack ){
+            InAttack = true;
+            AttackSelection++;
+            Vulnerable();
+            }
+        }
+
+		else if (State == BossState.Charging)
+        {
+            //Checks for player positions feeds into tackle
+            Direction = (PlayerNode.GlobalPosition - this.GlobalPosition).Normalized();
+            StaticDirection = Direction;
+            Animator.Set("parameters/Tackle/blend_position", StaticDirection);
+            Animator.Set("parameters/Charging/blend_position", StaticDirection);
+        }
+
+		else if(State == BossState.Tackle)
+        {
+            
+            Tackle(StaticDirection);
+        }
+
+        else if (State == BossState.Walking)
+        {
+            //Actual Melee Attacks not implemented yet
+            Direction = (PlayerNode.GlobalPosition - this.GlobalPosition).Normalized();
+			Velocity = 70 * Direction;
+			Animator.Set("parameters/Walking/blend_position", Direction);
+            MoveAndSlide();
+             
+        }
+
+        else if(State == BossState.Projectile)
+        {
+            
+        }
+
+        else if(State == BossState.Attacking)
+        {
+                
+        }
+
+        }
+	}
+
+
+
+
+	public bool IsIdle()
+    {
+        return State == BossState.Idle;
+    }
+	public bool IsProjectile()
+    {
+        return State == BossState.Projectile;
+    }
+	public bool IsWalking()
+    {
+        return State == BossState.Walking;
+    }
+	public bool IsAttacking()
+    {
+        return State == BossState.Attacking;
+    }
+	public bool IsCharging()
+    {
+        return State == BossState.Charging;
+    }
+	public bool IsTackle()
+    {
+        return State == BossState.Tackle;
+    }
+
+}
