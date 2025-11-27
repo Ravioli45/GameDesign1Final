381ca249afa7817378df0850607d7c507c9e8650
ARiveraRamirez <riveraalejandro2022@gmail.com>
2025-11-27 04:23:27 -0500

    Breakables implemented, with 3 variants: barrels, crates, and chests.

---
+++ Scripts/Breakable.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class Breakable : StaticBody2D
+{
+	[Export] PackedScene gold;
+    [Export] PackedScene healthOrb;
+    [Export] private int minGold = 0;
+    [Export] private int maxGold = 5;
+    [Export] private int minHealthOrb = 0;
+    [Export] private int maxHealthOrb = 1;
+
+    public void Break()
+    {
+        int amount_gold_drop = GD.RandRange(minGold, maxGold);
+        int amount_health_orb_drop = GD.RandRange(minHealthOrb, maxHealthOrb);
+        for (int i = 0; i < amount_gold_drop; i++)
+		{
+			Node2D Instance = gold.Instantiate<Node2D>();
+			Instance.Position = new Vector2(this.Position.X + GD.RandRange(-7, 7), this.Position.Y + GD.RandRange(-7, 7));
+			this.GetTree().Root.CallDeferred("add_child",Instance);
+		}
+        /*for (int i = 0; i < amount_health_orb_drop; i++)
+		{
+			Node2D Instance = healthOrb.Instantiate<Node2D>();
+			Instance.Position = new Vector2(this.Position.X + GD.RandRange(-7, 7), this.Position.Y + GD.RandRange(-7, 7));
+			this.GetTree().Root.CallDeferred("add_child",Instance);
+		}*/
+        GD.Print("Health Orbs can't spawn yet");
+        CallDeferred("queue_free");
+    }
+}
